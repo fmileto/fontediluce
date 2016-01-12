079d5015e91ecb5b6702f998f5a46b57dc802918
fmileto <mileto.francesco@gmail.com>
2016-01-12 13:19:39 +0100

    first commit

--- deploy.cmd
+++ deploy.cmd
@@ -100,17 +100,23 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install
+  call :ExecuteCmd npm install
   IF !ERRORLEVEL! NEQ 0 goto error
-  popd
 )
 
+echo Installing bower packages: Starting %TIME%
+call :ExecuteCmd "bower" install
+echo Installing bower packages: Finished %TIME%
+IF !ERRORLEVEL! NEQ 0 goto error
+
+
 :: 4. Build the webclient
-IF EXIST "%DEPLOYMENT_TARGET%\Gulpfile.js" (
-  pushd "%DEPLOYMENT_TARGET%"
-  echo "Building web site using Gulp"
-  call :ExecuteCmd ".\node_modules\.bin\gulp.cmd"
-  if !ERRORLEVEL! NEQ 0 goto error
+IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
+  echo Running Gulp: Starting %TIME%
+  call :ExecuteCmd "gulp build"
+  echo Running Gulp: Finished %TIME%
+  
+  IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
 
